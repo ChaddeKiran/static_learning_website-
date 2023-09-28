5c2f6f03f7f22765227d43c7b0f2d44991ab73a4
Kiran Chadde <87512557+ChaddeKiran@users.noreply.github.com>
2023-09-28 19:55:11 +0530

    Updated the source path in file provisioner method  resource.tf
    source = "Dockerfile"
    source= "app.py"

--- resource.tf
+++ resource.tf
@@ -17,18 +17,18 @@ resource "aws_instance" "tf_t2_micro_instance" {
   connection {
     type        = "ssh"
     user        = "ubuntu"              # Replace with the appropriate username for your EC2 instance
-    private_key = file("${path.module}/id_rsa.pub") # Replace with the path to your private key
+    private_key = file("id_rsa.pub") # Replace with the path to your private key
     host        = self.public_ip
   }
 
   # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
-    source      = "/workspaces/DevOps_notes_repo/terraform-project/terraform_task/Dockerfile" # Replace with the path to your local file
+    source      = "Dockerfile" # Replace with the path to your local file
     destination = "/home/ubuntu/Dockerfile"                                                             # Replace with the path on the remote instance
   }
 
     provisioner "file" {
-    source      = "/workspaces/DevOps_notes_repo/terraform-project/terraform_task/app.py" # Replace with the path to your local file
+    source      = "app.py"
     destination = "/home/ubuntu/app.py"                                                             # Replace with the path on the remote instance
   }
 
