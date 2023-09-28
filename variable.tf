b6d6191177ab8619fb4b46c242fd7203b5390fb8
Kiran Chadde <87512557+ChaddeKiran@users.noreply.github.com>
2023-09-28 20:03:16 +0530

    Updated variable.tf
    aws access key and secret key variable is defined

--- variable.tf
+++ variable.tf
@@ -1,3 +1,10 @@
 variable "cidr" {
   default = "10.0.0.0/16"
 }
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
