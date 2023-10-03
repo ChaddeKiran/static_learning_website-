25896f3e563a0e38f222a05db06f3b5f32954b2d
Kiran Chadde <87512557+ChaddeKiran@users.noreply.github.com>
2023-10-03 13:37:21 +0530

    Update provider.tf
    uncomment provider

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1" # Replace with your desired AWS region.
- // access_key = var.aws_access_key
- // secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
